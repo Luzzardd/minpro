9529a36b3baa8e2f43c06b5d3de8ea83a95cee87
Luzzardd <mnaufalhasyim112@gmail.com>
2025-01-20 20:00:46 +0700

    merge login register and homepage

---
+++ apps/web/src/components/HeaderNoLogin.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+export const HeaderOnly = () => {
+    const router = useRouter()
+
+    const handleRedirect = () => { router.push('/Login'); // Replace with your desired path };
+  }
+};
+  
+
+  
