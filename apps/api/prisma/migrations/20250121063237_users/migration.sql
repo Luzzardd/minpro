9e8d5cbadc9f6a23b1b782728aeb2034f9bb2584
Luzzardd <mnaufalhasyim112@gmail.com>
2025-01-21 14:16:16 +0700

    update backend

--- apps/api/prisma/migrations/20250121063237_users/migration.sql
+++ apps/api/prisma/migrations/20250121063237_users/migration.sql
@@ -21,5 +21,21 @@ CREATE TABLE "users" (
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "events" (
+    "event_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("event_id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "samples_code_key" ON "samples"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
